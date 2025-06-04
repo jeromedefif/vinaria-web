4937c05f1add3da575395ad47b626c2ed2978460
jeromedefif <fiala@vinaria.cz>
2025-06-04 09:47:48 +0200

    Smartsupp doplnění page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,20 @@ export default function Home() {
         <HeroSection />
         <BusinessAreas />
 
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '5f252c61af225db2374bdc9a91f59b4e717d7f47';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
+        
         <section id="katalog" className="py-16 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <ProductCategories
